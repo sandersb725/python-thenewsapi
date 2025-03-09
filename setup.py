a56d42d5e621d715692576a7d290c359ad799413
Brighton Sanders <136634756+sandersb725@users.noreply.github.com>
2025-03-09 04:47:54 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,27 @@
 try:
-  from setuptools import setup
+	from setuptools import setup
 except ImportError:
-  from distutils.core import setup
+	from distutils.core import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+with open('README.md', 'r') as f:
+	long_description = f.read()
 
 setup(
-    name="python-thenewsapi",
-    version="0.0.1",
-    author="Brighton Sanders",
-    author_email="bus120610@gmail.com",
-    description="A python wrapper for thenewsapi.com",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/sandersb725/python-thenewsapi",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+	name='python-thenewsapi',
+	packages=['thenewsapi'],
+	version='0.0.1',
+	description='A python wrapper for thenewsapi.com',
+	author='Brighton Sanders',
+	url='https://github.com/sandersb725/python-thenewsapi',
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	author_email='bus120610@gmail.com',
+	classifiers = [
+		'Intended Audience :: End Users/Desktop',
+		'Intended Audience :: Developers',
+		'Intended Audience :: Information Technology',
+		'Intended Audience :: System Administrators',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 3'
+	],
 )
